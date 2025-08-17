f14d754ca90e0df857d98e4dd37ee7388e5f9650
Shivanshu <shivanshusharmasks@gmail.com>
2025-08-17 12:42:07 +0530

    commit2

--- gitpractice.cpp
+++ gitpractice.cpp
@@ -1,8 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+int fact(int n){
+    if(n<=1)
+    return 1;
+    return n*fact(n-1);
+}
 int main(){
     int n;
-    cout<<"enter a number:";
+    cout<<"enter a number to print fact:";
     cin>>n;
-    cout<<"you entered a number:"<<n<<endl;
+    int y=fact(n);
+    cout<<"factorial of the  number:"<<y<<endl;
 }
