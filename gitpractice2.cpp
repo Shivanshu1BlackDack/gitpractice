8eb805cb62ca463b7f7ee58f454612a9ec715d60
Shivanshu <shivanshusharmasks@gmail.com>
2025-08-17 13:01:53 +0530

    change 2 in file2

--- gitpractice2.cpp
+++ gitpractice2.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 int binconvert(int n){
     int num=0;
-    while(n>=0){
+    while(n>0){
         int y=n%2;
-        num*=10+y;
+        num=num*10+y;
         n/=2;
     }
     return num;
 }
 int main(){
     int x;
+    cin>>x;
+    int y=binconvert(x);
+    cout<<"your binary conversion is:"<<y<<endl;
+    return 0;
 }
